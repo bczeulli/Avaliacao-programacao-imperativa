2bc4fd3c06aee69669e61a7bfdc7695ecc9e5c38
Bruno Cesar Zeulli <bczeulli@gmail.com>
2021-12-09 17:03:35 -0300

    quase finalizado

--- aluno/index.js
+++ aluno/index.js
@@ -3,7 +3,7 @@ function Aluno(nome, faltas, notas) {
     this.nome = nome;
     this.qtdDeFaltas = faltas;
     this.notas = notas;
-    this.media = function calcularMedia() {
+    this.media = function () {
         let soma = 0
         for(let i = 0; i < this.notas.length; i++) {
             soma = soma + this.notas[i]
@@ -12,16 +12,15 @@ function Aluno(nome, faltas, notas) {
         return soma / this.notas.length
     };
 
-    this.faltas = function somarFaltas(){
-        let faltasTotais = 1;
-        faltasTotais += this.qtdDeFaltas
-        return faltasTotais
+    this.faltas = function () {
+        return this.qtdDeFaltas + 1
     }
+
     
     
 }
 
-const aluno1 = new Aluno("Bruno",8,[5,8,7,9]);
+const aluno1 = new Aluno("Bruno",8,[10,8,7,9]);
 const aluno2 = new Aluno("Adriana",15,[6,9,3,10]);
 const aluno3 = new Aluno("Jose",4,[8,6,10,8]);
 const aluno4 = new Aluno("Maria",7,[7,8,9,8]);
@@ -29,18 +28,9 @@ const aluno5 = new Aluno("Ricardo",12,[7,8,6,5]);
 
 
 module.exports = {
-    novoAluno: function(nome, faltas, notas) {
-        const novoAluno = new Aluno(nome, faltas, notas);
-        this.alunos.push(novoAluno);
+    listaAlunos: [aluno1, aluno2, aluno3, aluno4, aluno5],
+    novoAluno: function(nome, faltas, notas){
+        const aluno = new Aluno (nome, faltas, notas)
+        this.listaAlunos.push(aluno)
     },
-    alunos: [aluno1, aluno2, aluno3, aluno4, aluno5],
-    mediaNotas: function () {
-        let arr = this.alunos [0]
-        let soma = 0;
-        for(let i = 0; i <this.alunos.notas.length; i ++ ) {
-            soma = soma + this.alunos.notas[i]
-        } return soma / this.alunos.notas.length 
-        
-    },
-    
 }
---
+++ curso.js
@@ -0,0 +1,52 @@
+let alunos = require ('./aluno')
+
+function Curso(nomeDoCurso, notaDeAprovacao, faltasMaximas) {
+    this.nomeDoCurso = nomeDoCurso ;
+    this.notaDeAprovacao = notaDeAprovacao ;
+    this.faltasMaximas = faltasMaximas ;
+    this.listaDeEstudantes = alunos.listaAlunos ;
+
+    this.adicionarAluno = function (nome, faltas, notas) {
+        const novoAluno = alunos.novoAluno(nome, faltas, notas);
+        return this.listaDeEstudantes.push(novoAluno) ;
+    }
+
+    this.aprovarAluno = function (alunoAvaliado) {
+        let aprovado
+        if (alunoAvaliado.media() >= this.notaDeAprovacao && alunoAvaliado.qtdDeFaltas < this.faltasMaximas) {
+            
+            return aprovado = true
+        } else if (alunoAvaliado.media() >= (this.notaDeAprovacao + (this.notaDeAprovacao * 0.1)) && alunoAvaliado.qtdDeFaltas == this.faltasMaximas) {
+            
+            return aprovado = true
+        } else {
+            return aprovado = false
+        }
+    }
+    
+                
+    
+
+    this.alunosAprovados = function () {
+        
+        let listaAlunosAprovados = []
+        listaAlunosAprovados.push(this.aprovarAluno(this.listaDeEstudantes[0]))
+        listaAlunosAprovados.push(this.aprovarAluno(this.listaDeEstudantes[4]))
+        listaAlunosAprovados.push(this.aprovarAluno(this.listaDeEstudantes[5]))
+        console.log(listaAlunosAprovados)
+    }
+    
+
+
+
+
+
+}
+
+
+
+let prog = new Curso("prog", 7, 20)
+
+prog.adicionarAluno("Henrique", 9, [2,5,3,6]);
+
+prog.alunosAprovados()
